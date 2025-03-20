5625330f67bfbd3d423573aa34c4947519c7d5b5
mshiray <10815743+mshiray@users.noreply.github.com>
2025-03-20 13:50:12 +0530

    launch app name, vs code profile file and hello.cpp comments

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
+
+//VS Code:
+  //to build: Cntrl+Shift+B
+  //to run: Cntrl+F5
+  //change  "program": "${workspaceFolder}/hello" in launch.json
+  // import c/c++ code profile under .vscode folder to setup VS Code editor with required plugins and configurations
+  //and select it as the profile for the current workspace.
+
+//VIM: Press F8 ro compile-run
+//Make sure .vimrc in PWD
+
+
 using namespace std;
-//to build: Cntrl+Shift+B
-//to run: Cntrl+F5
+
 int main() {
-    //to run with cntrl+F5 =>
-   //change  "program": "${workspaceFolder}/hello" in launch.json
+
     cout << "Hello cpp!\n";
     return 0;
 }
